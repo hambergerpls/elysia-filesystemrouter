4c58589f80121f2c649e9f953f5bdf2ed683bd0e
Danial Hanif <danialhanif@outlook.com>
2024-01-26 14:38:57 +0800

    chore(*): update .gitignore

--- test/routes/delete/index.g.ts
+++
@@ -1,7 +0,0 @@
-
-import Elysia, { Handler, InputSchema, LocalHook, MergeSchema, UnwrapRoute } from "elysia";
-import { BasePath, Decorators, Definitions, Macro, ParentSchema } from "elysia-filesystemrouter";
-
-export function Handler<ElysiaApp extends Elysia, const Path extends string, const LocalSchema extends InputSchema<keyof Definitions<ElysiaApp>['type'] & string>, const Route extends MergeSchema<UnwrapRoute<LocalSchema, Definitions<ElysiaApp>['type']>, ParentSchema<ElysiaApp>>, const Handle extends Exclude<Route['response'], Handle> | Handler<Route, Decorators<ElysiaApp>, `${BasePath<ElysiaApp>}/delete`>>(app: ElysiaApp, handler: Handle, hook?: LocalHook<LocalSchema, Route, Decorators<ElysiaApp>, Definitions<ElysiaApp>['error'], Macro<ElysiaApp>, `${BasePath<ElysiaApp>}/delete`>) {
-    return { path: '/delete' , handler, hook }
-}
